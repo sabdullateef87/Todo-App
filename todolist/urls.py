f572a7ec405d065c103f2391ecf28216f18325d2
Abdullateef.Suleiman <abdullateef.suleiman@interswitchng.com>
2024-05-02 09:33:46 +0100

    Salama K commit

---
+++ todolist/urls.py
@@ -0,0 +1,7 @@
+from . import views
+from django.urls import path
+
+urlpatterns = [
+  path("", views.index, name="index"),
+  path("category", views.category, name="category")
+]
---
+++ todolist/views.py
@@ -0,0 +1,61 @@
+from django.http import Http404, HttpResponse
+from django.shortcuts import render
+
+# Create your views here.
+from django.shortcuts import render,redirect
+from .models import TodoList, Category
+
+urls = {
+    "category_url" : 'http://127.0.0.1:8000/todos/category',
+    "todo_url": 'http://127.0.0.1:8000/todos',
+}
+def index(request): #the index view
+    todos = TodoList.objects.all() #quering all todos with the object manager
+    categories = Category.objects.all() #getting all categories with object manager
+
+    # for category in categories:
+    #     category.delete()
+    # Category.objects.get(name='Eating').delete()
+    if request.method == "POST": #checking if the request method is a POST
+        print("I just clicked the buttommaaaaaaaaaaaaaaa")
+        if "taskAdd" in request.POST: #checking if there is a request to add a todo
+            title = request.POST["description"] #title
+            date = str(request.POST["date"]) #date
+            category = request.POST["category_select"] #category
+            content = title + " -- " + date + " " + category #content
+            Todo = TodoList(title=title, content=content, due_date=date, category=Category.objects.get(name=category))
+            Todo.save() #saving the todo 
+            return redirect("/todos") #reloading the page
+        if "taskDelete" in request.POST: #checking if there is a request to delete a todo
+            checkedlist = request.POST["checkedbox"] #checked todos to be deleted
+            for todo_id in checkedlist:
+                todo = TodoList.objects.get(id=int(todo_id)) #getting todo id
+                todo.delete() #deleting todo
+
+    return render(request, "index.html", {"todos": todos,  "categories":categories, "url": urls})
+
+def category(request):
+    categories = Category.objects.all()
+    if request.method == 'POST':
+        if "addCategory" in request.POST:
+            print("This is a create category post request", request.POST)
+            category_name = request.POST['category']
+            if Category.objects.filter(name=category_name).exists():
+                raise Http404("Category already exists")
+            new_category = Category(name=category_name)
+            new_category.save()
+            return redirect("/todos/category")
+        
+        if "deleteCategory" in request.POST:
+            category_id = int(request.POST.get("deleteCategory"))
+            category_to_delete = Category.objects.get(id=category_id)
+            category_to_delete.delete()
+            return redirect("/todos/category")
+    context = {
+        "categories" : categories,
+        "url": urls
+    }
+    return render(request, "category.html", context)
+
+
+    # return render(request, "category.html", {"categories" : categories, "url" : urls})
